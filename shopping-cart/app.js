17f51bc4a359d52921401eeb98be2093b73da715
karina-vopm <kmontero@vopm.net>
2022-01-31 17:00:49 -0400

    Working

--- shopping-cart/app.js
+++ shopping-cart/app.js
@@ -1,14 +1,44 @@
-const container = document.querySelector(".fruit");
-let imageTag;
+//UPLOAD AND SHOW PHOTOS FROM PEXELS API
+const containerProducts = document.querySelector(".products");
+const categoryName = document.getElementById('category-name');
+const categories = Array.from(document.getElementsByClassName("category"));
 
-function getPhotos(images, category = "fruit"){
-    images.map(image => {
-        imageTag += `<div class="image">
-                        <img src="${image.src.tiny}" alt="${image.alt}">
-                    </div>`;
-    });
-    container.innerHTML = imageTag;
+function getPhotos(images){
+        let imageTag = "";
+        images.map(image => {
+            imageTag += `<div class="product" id="${image.id}">
+                             <div class="image">
+                                 <img src="${image.src.tiny}" alt="${image.alt}">
+                             </div>
+                             <div class="description">
+                                 <p>${image.alt}</p>
+                                 <p>RD$ 100.00</p>
+                                 <button>Añadir al carrito</button>
+                                 <button>Comprar <i class="fas fa-shopping-cart"></i></button>
+                                 <span><i class="fas fa-cart-plus"></i><span/>
+                                 <span><i class="fas fa-heart"></i></span>
+                             </div>
+                         </div>`;
+        });
+        containerProducts.innerHTML = imageTag;
 }
 
+async function pexelsApi(search, quantity=10){
+    await fetch(`https://api.pexels.com/v1/search?query=${search}&per_page=${quantity}`,{
+        headers: {
+          Authorization: "563492ad6f91700001000001c9529b304f5d4f93b814f807ebaf9860"
+        }
+    })
+        .then(resp => {return resp.json()})
+        .then(data => {
+            getPhotos(data.photos);
+        })
+}
 
-//getPhotos()
+categories.forEach(category => {
+    category.addEventListener('click', function(){
+        categoryName.innerText = this.innerText;
+        let category = this.id;
+        pexelsApi(category);
+    })
+})
