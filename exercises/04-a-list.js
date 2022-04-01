2e375ca42337b89687e4c59702e9932626035ead
Karina Montero <karina_0711_@hotmail.com>
2022-04-01 00:54:56 -0400

    Excercises of Chapter 4 In Progress

---
+++ exercises/04-a-list.js
@@ -0,0 +1,48 @@
+/*
+Escribe una función arrayALista que construya una estructura de lista como el que se muestra
+arriba cuando se le da [1, 2, 3] como argumento. También escribe una función listaAArray
+que produzca un array de una lista. Luego agrega una función de utilidad preceder, que tome
+un elemento y una lista y creé una nueva lista que agrega el elemento al frente de la lista
+de entrada, y posicion, que toma una lista y un número y retorne el elemento en la posición
+dada en la lista (con cero refiriéndose al primer elemento) o undefined cuando no exista tal
+elemento.
+
+Si aún no lo has hecho, también escribe una versión recursiva de posicion.
+
+// Tu código aquí.
+
+console.log(arrayALista([10, 20]));
+// → {valor: 10, resto: {valor: 20, resto: null}}
+console.log(listaAArray(arrayALista([10, 20, 30])));
+// → [10, 20, 30]
+console.log(preceder(10, preceder(20, null)));
+// → {valor: 10, resto: {valor: 20, resto: null}}
+console.log(posicion(arrayALista([10, 20, 30]), 1));
+// → 20
+*/
+
+let test = {
+    valor: 10, 
+    resto: {
+        valor: 20, 
+        resto: null
+    }
+}
+
+const arrayToList  = (arr) => {
+    let json = {};
+    for(let i = 0; i < arr.length; i++){
+        json['valor'] = arr[i];
+        console.log(arr[i+1]);
+        json['resto'] = arr[i+1] == undefined ? "Hola" : null;
+    }
+    return json;
+}
+
+test2 = [10,20];
+console.log(test2.splice(1))
+
+console.log(arrayToList([10, 20]));
+
+const listToArray = () => {
+}
