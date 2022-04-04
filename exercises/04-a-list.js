97adf953140ec161e9bb244b88ff149d8cb72e45
Karina Montero <karina_0711_@hotmail.com>
2022-04-03 22:00:42 -0400

    Excercises of Chapter 4 In Progress

--- exercises/04-a-list.js
+++ exercises/04-a-list.js
@@ -1,9 +1,12 @@
 /*
 Escribe una función arrayALista que construya una estructura de lista como el que se muestra
 arriba cuando se le da [1, 2, 3] como argumento. También escribe una función listaAArray
-que produzca un array de una lista. Luego agrega una función de utilidad preceder, que tome
+que produzca un array de una lista. 
+
+Luego agrega una función de utilidad preceder, que tome
 un elemento y una lista y creé una nueva lista que agrega el elemento al frente de la lista
-de entrada, y posicion, que toma una lista y un número y retorne el elemento en la posición
+de entrada, y 
+posicion, que toma una lista y un número y retorne el elemento en la posición
 dada en la lista (con cero refiriéndose al primer elemento) o undefined cuando no exista tal
 elemento.
 
